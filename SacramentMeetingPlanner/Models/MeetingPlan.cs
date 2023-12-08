0a3b5e03266f4e44c595e337f23cd85f2bde3d19
LINDSEY FUND <LINFUND@msn.com>
2023-12-07 19:52:10 -0500

    Validation

--- SacramentMeetingPlanner/Models/MeetingPlan.cs
+++ SacramentMeetingPlanner/Models/MeetingPlan.cs
@@ -9,34 +9,42 @@ namespace SacramentMeetingPlanner.Models
         [Display(Name = "Meeting Date")]
         [DataType(DataType.DateTime)]
         public DateTime MeetingDate { get; set; }
-        
+
+        [StringLength(50)]
         [Display(Name = "Conducting")]
         public string? ConductingLeader { get; set; }
 
+        [StringLength(50)]
         [Display(Name = "Presiding")]
         public string? PresidingLeader { get; set; }
 
-        [Display(Name = "Opening Hymn")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Display(Name = "Opening Hymn (Title - number)")]
         public string? OpeningHymn {  get; set; }
 
+        [StringLength(50)]
         [Display(Name = "Opening Prayer")]
         public string? OpeningPrayer { get; set; }
 
         [Display(Name = "Stake/Ward/Branch Business")]
         public string? Buisness { get; set; }
 
-        [Display(Name = "Sacrament Hymn")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Display(Name = "Sacrament Hymn (Title - number)")]
         public string? SacramentHymn { get; set; }
 
-
+        [StringLength(100)]
         public string? Speaker {  get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         [Display(Name = "Musical Number (optional)")]
         public string? MusicalNumber { get; set; }
 
-        [Display(Name = "Closing Hymn")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Display(Name = "Closing Hymn (Title - number)")]
         public string? ClosingHymn { get; set; }
 
+        [StringLength(50)]
         [Display(Name = "Closing Prayer")]
         public string? ClosingPrayer { get; set; }
         
