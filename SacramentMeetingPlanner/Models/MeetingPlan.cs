fd93698a70d824f5efc1c41dafd282874c643c1e
LINDSEY FUND <LINFUND@msn.com>
2023-12-07 19:27:36 -0500

    edited displays for the meeting plan index and creatte pages

--- SacramentMeetingPlanner/Models/MeetingPlan.cs
+++ SacramentMeetingPlanner/Models/MeetingPlan.cs
@@ -5,17 +5,39 @@ namespace SacramentMeetingPlanner.Models
     public class MeetingPlan
     {
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Meeting Date")]
         [DataType(DataType.Date)]
         public DateTime MeetingDate { get; set; }
+        
+        [Display(Name = "Conducting")]
         public string? ConductingLeader { get; set; }
+
+        [Display(Name = "Presiding")]
         public string? PresidingLeader { get; set; }
+
+        [Display(Name = "Opening Hymn")]
         public string? OpeningHymn {  get; set; }
+
+        [Display(Name = "Opening Prayer")]
         public string? OpeningPrayer { get; set; }
+
+        [Display(Name = "Stake/Ward/Branch Business")]
         public string? Buisness { get; set; }
+
+        [Display(Name = "Sacrament Hymn")]
         public string? SacramentHymn { get; set; }
+
+
         public string? Speaker {  get; set; }
+
+        [Display(Name = "Musical Number (optional)")]
         public string? MusicalNumber { get; set; }
+
+        [Display(Name = "Closing Hymn")]
         public string? ClosingHymn { get; set; }
+
+        [Display(Name = "Closing Prayer")]
         public string? ClosingPrayer { get; set; }
         
 
